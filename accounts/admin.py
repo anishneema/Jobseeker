a89842f09ddb9ae6cd4b7505ab1443ecf67981b1
Michelle Hu <michellehu8888@gmail.com>
2026-09-18 17:06:37 -0400

    user stories 1 and 5 - job seeker profile, privacy settings

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,4 +1,19 @@
 from django.contrib import admin
-from .models import RecruiterProfile
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
+from .models import RecruiterProfile, JobSeekerProfile
 
+class CustomUserAdmin(UserAdmin):
+    list_display = UserAdmin.list_display + ('role',)
+
+    def role(self, user):
+        if hasattr(user, 'recruiter_profile'):
+            return 'Recruiter'
+        if hasattr(user, 'jobseeker_profile'):
+            return 'Job Seeker'
+        return '-'
+
+admin.site.unregister(User)
+admin.site.register(User, CustomUserAdmin)
 admin.site.register(RecruiterProfile)
+admin.site.register(JobSeekerProfile)
--- accounts/context_processors.py
+++ accounts/context_processors.py
@@ -1,5 +1,7 @@
 def role(request):
     is_recruiter = False
+    is_seeker = False
     if request.user.is_authenticated:
         is_recruiter = hasattr(request.user, 'recruiter_profile')
-    return {'is_recruiter': is_recruiter}
+        is_seeker = hasattr(request.user, 'jobseeker_profile')
+    return {'is_recruiter': is_recruiter, 'is_seeker': is_seeker}
