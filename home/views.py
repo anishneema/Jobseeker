6d59667df82f424f79656f2e4e259caaff7b2147
Isabel-Su <isabelsu2025@gmail.com>
2026-09-18 14:51:21 -0400

    name fix

--- home/views.py
+++ home/views.py
@@ -3,6 +3,6 @@ from django.shortcuts import render
 
 def index(request):
     template_data = {}
-    template_data['title'] = 'Job Platform'
+    template_data['title'] = 'JobSeeker'
     return render(request, 'home/index.html',
                   {'template_data': template_data})
